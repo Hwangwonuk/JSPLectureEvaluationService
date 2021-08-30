63d5bc55c77528089987a93bb3e1cdbc9c1ecd9e
Hwangwonuk <86584999+Hwangwonuk@users.noreply.github.com>
2021-08-30 09:54:26 +0900

    UserDAO, DTO, DBUtil 추가
    DB와 연동되는 JAVA

---
+++ LectureEvaluation/src/user/UserDAO.java
@@ -0,0 +1,144 @@
+package user;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import util.DatabaseUtil;
+
+public class UserDAO {
+
+	public int login(String userID, String userPassword) { // 로그인 함수 정의
+		String SQL = "SELECT userPassword FROM USER WHERE userID = ?";
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			// DB접근하는 함수는 DatabaseUtil 외부 util에 정의를 함으로써 안정적으로 모듈화 함
+			conn = DatabaseUtil.getConnection();	// DB와 연결
+			pstmt = conn.prepareStatement(SQL);		// SQL실행 문자로 준비시킴
+			pstmt.setNString(1,  userID);			// 사용자로부터 입력받은  ID값을 첫번째 물음표에 넣어줌
+			rs = pstmt.executeQuery();				// DB실행문을 실행시킨 결과를 담음
+			if (rs.next()) {						// 실행결과가 존재하는 경우에 한해서
+				if (rs.getNString(1).contentEquals(userPassword)) {	
+					return 1; // 로그인 성공
+				}
+				else {
+					return 0; // 비밀번호 틀림
+				}
+			}
+			return -1; // 아이디 없음
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			// Connection, PreparedStatement, ResultSet은 한번 사용이 되고나면 자원을 해제하여야 한다.
+			// 서버에 무리가 가지 않도록 하기위해서
+			try { if (conn != null) conn.close(); } catch (Exception e) { e.printStackTrace(); }
+			try { if (pstmt != null) pstmt.close(); } catch (Exception e) { e.printStackTrace(); }
+			try { if (rs != null) rs.close(); } catch (Exception e) { e.printStackTrace(); }
+		}
+		return -2; // 데이터베이스 오류
+	}
+	
+	public int join(UserDTO user) { // 사용자의 정보를 입력받아 회원가입 수행
+		String SQL = "INSERT INTO USER VALUES (?, ?, ?, ?, false)"; // id, pwd, email, hash, emailchecked
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			conn = DatabaseUtil.getConnection();	// DB와 연결
+			pstmt = conn.prepareStatement(SQL);		// SQL실행 문자로 준비시킴
+			pstmt.setString(1,  user.getUserID());			
+			pstmt.setString(2,  user.getUserPassword());
+			pstmt.setString(3,  user.getUserEmail());
+			pstmt.setString(3,  user.getUserEmailHash());
+			return pstmt.executeUpdate();			// 실제로 반영된 데이터의 개수
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			// Connection, PreparedStatement, ResultSet은 한번 사용이 되고나면 자원을 해제하여야 한다.
+			// 서버에 무리가 가지 않도록 하기위해서
+			try { if (conn != null) conn.close(); } catch (Exception e) { e.printStackTrace(); }
+			try { if (pstmt != null) pstmt.close(); } catch (Exception e) { e.printStackTrace(); }
+			try { if (rs != null) rs.close(); } catch (Exception e) { e.printStackTrace(); }
+		}
+		return -1; // 회원가입 실패
+	}
+	
+	public String getUserEmail(String userID) { // 사용자의 ID값을 받아서 사용자의 Email주소를 받아감
+			String SQL = "SELECT userEmail FROM USER WHERE userID = ?"; // id, pwd, email, hash, emailchecked
+			Connection conn = null;
+			PreparedStatement pstmt = null;
+			ResultSet rs = null;
+			try {
+				conn = DatabaseUtil.getConnection();	// DB와 연결
+				pstmt = conn.prepareStatement(SQL);		// SQL실행 문자로 준비시킴
+				pstmt.setString(1, userID);
+				rs = pstmt.executeQuery();
+				if (rs.next()) { 						// 존재하는 아이디일 경우
+					return rs.getString(1);
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			} finally {
+				// Connection, PreparedStatement, ResultSet은 한번 사용이 되고나면 자원을 해제하여야 한다.
+				// 서버에 무리가 가지 않도록 하기위해서
+				try { if (conn != null) conn.close(); } catch (Exception e) { e.printStackTrace(); }
+				try { if (pstmt != null) pstmt.close(); } catch (Exception e) { e.printStackTrace(); }
+				try { if (rs != null) rs.close(); } catch (Exception e) { e.printStackTrace(); }
+			}
+			return null; // 데이터베이스 오류
+	}
+	
+	// 이메일 인증이 되지 않으면, 강의평가를 작성할 수 없음
+	public boolean getUserEmailChecked (String userID) { // 사용자의 아이디값을 입력받아 사용자가 이메일 검증이 되었는지 확인해야함
+		String SQL = "SELECT userEmailChecked FROM USER HERE userID = ?"; // id, pwd, email, hash, emailchecked
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			conn = DatabaseUtil.getConnection();	// DB와 연결
+			pstmt = conn.prepareStatement(SQL);		// SQL실행 문자로 준비시킴
+			pstmt.setString(1, userID);
+			rs = pstmt.executeQuery();
+			if (rs.next()) { 						// 존재하는 아이디일 경우
+				return rs.getBoolean(1);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			// Connection, PreparedStatement, ResultSet은 한번 사용이 되고나면 자원을 해제하여야 한다.
+			// 서버에 무리가 가지 않도록 하기위해서
+			try { if (conn != null) conn.close(); } catch (Exception e) { e.printStackTrace(); }
+			try { if (pstmt != null) pstmt.close(); } catch (Exception e) { e.printStackTrace(); }
+			try { if (rs != null) rs.close(); } catch (Exception e) { e.printStackTrace(); }
+		}
+		return false; // 데이터베이스 오류
+	}
+	
+	
+	// 이메일 인증이 되지 않으면, 강의평가를 작성할 수 없음
+	public boolean setUserEmailChecked (String userID) { // 특정한 사용자가 검증을통해 인증을 해주는 함수
+		String SQL = "UPDATE USER SET userEmailChecked = true WHERE userID = ?";
+		// userEmailChecked의 값을 true로 바꿔준다 이메일 인증되도록 처리
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			conn = DatabaseUtil.getConnection();	// DB와 연결
+			pstmt = conn.prepareStatement(SQL);		// SQL실행 문자로 준비시킴
+			pstmt.setString(1, userID);
+			pstmt.executeUpdate();
+			return true; // 인증이 된 경우라도 한번 더 인증
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			// Connection, PreparedStatement, ResultSet은 한번 사용이 되고나면 자원을 해제하여야 한다.
+			// 서버에 무리가 가지 않도록 하기위해서
+			try { if (conn != null) conn.close(); } catch (Exception e) { e.printStackTrace(); }
+			try { if (pstmt != null) pstmt.close(); } catch (Exception e) { e.printStackTrace(); }
+			try { if (rs != null) rs.close(); } catch (Exception e) { e.printStackTrace(); }
+		}
+		return false; // 데이터베이스 오류
+	}
+}
---
+++ LectureEvaluation/src/util/DatabaseUtil.java
@@ -0,0 +1,26 @@
+package util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DatabaseUtil { // 실질적으로 데이터베이스와 연동하는 부분
+	
+	public static Connection getConnection() {
+		try {
+			String dbURL = "jdbc:mysql://localhost:3306/LectureEvaluation?characterEncoding=UTF-8&serverTimezone=UTC"; // 연동할 mysql
+			String dbID = "root"; // 최고 권한 아이디
+			String dbPassword = "111111"; // 권한 아이디의 비밀번호
+			Class.forName("com.mysql.cㅊj.jdbc.Driver"); // com.mysql.jdbc.Driver라는 클래스를 찾아서 사용하겠다.
+			// 빌드했더니 아래 메시지 나와서 클래스 패키지명 com.mysql.cj.jdbc.Driver로 바꿈
+			// Loading class `com.mysql.jdbc.Driver'. This is deprecated.
+			// The new driver class is `com.mysql.cj.jdbc.Driver'.
+			// The driver is automatically registered via the SPI and manual loading of the driver class
+			// is generally unnecessary.
+			
+			return DriverManager.getConnection(dbURL, dbID, dbPassword); // 접속한 상태 자체를 반환
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null; // 오류가 발생하면 null값을 반환
+	}
+}
