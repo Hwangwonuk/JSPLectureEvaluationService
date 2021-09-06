cf7625e9a48a34ff66a319b9eca3f2c127b9bf1a
Hwangwonuk <86584999+Hwangwonuk@users.noreply.github.com>
2021-09-07 00:38:13 +0900

    아이디 비밀번호 수정

--- LectureEvaluation/src/util/Gmail.java
+++ LectureEvaluation/src/util/Gmail.java
@@ -7,7 +7,7 @@ public class Gmail extends Authenticator {
 	
 	@Override
 	protected PasswordAuthentication getPasswordAuthentication() {
-		return new PasswordAuthentication("hok0173@gmail.com", "vpfmthsk3!"); 
+		return new PasswordAuthentication("이메일 아이디", "비밀번호"); 
 		// 메일을 보낼 관리자 자신의 Gmail 계정의 id,pwd
 		// google 계정 보안에 들어가서 보안 수준이 낮은 앱의 액세스 허용 체크해야한다. 구글입장에서는 낮은 보안이기 때문
 	}
